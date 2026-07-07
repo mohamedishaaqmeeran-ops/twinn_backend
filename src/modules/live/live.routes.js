b2e6133898d07187624f52ab73687e34ad4c222e
unknown <mohamedishaaqmeeran@gmail.com>
2026-07-07 21:54:41 +0530

    updates

--- src/modules/live/live.routes.js
+++ src/modules/live/live.routes.js
@@ -2,32 +2,28 @@ const express = require("express");
 const router = express.Router();
 
 const liveController = require("./live.controller");
-const authMiddleware = require("../../middleware/auth.middleware");
+const { protect } = require("../../middleware/auth.middleware");
 
-router.post("/upload-video", authMiddleware, liveController.uploadVideo);
+if (typeof protect !== "function") {
+  throw new Error("protect middleware is not a function");
+}
 
-router.post(
-  "/start-instagram-rtmp",
-  authMiddleware,
-  liveController.startInstagramRTMP
-);
+[
+  "uploadVideo",
+  "startInstagramRTMP",
+  "stopInstagramRTMP",
+  "startFacebookLive",
+  "stopFacebookLive",
+].forEach((fn) => {
+  if (typeof liveController[fn] !== "function") {
+    throw new Error(`liveController.${fn} is not a function`);
+  }
+});
 
-router.post(
-  "/stop-instagram-rtmp",
-  authMiddleware,
-  liveController.stopInstagramRTMP
-);
-
-router.post(
-  "/start-facebook",
-  authMiddleware,
-  liveController.startFacebookLive
-);
-
-router.post(
-  "/stop-facebook",
-  authMiddleware,
-  liveController.stopFacebookLive
-);
+router.post("/upload-video", protect, liveController.uploadVideo);
+router.post("/start-instagram-rtmp", protect, liveController.startInstagramRTMP);
+router.post("/stop-instagram-rtmp", protect, liveController.stopInstagramRTMP);
+router.post("/start-facebook", protect, liveController.startFacebookLive);
+router.post("/stop-facebook", protect, liveController.stopFacebookLive);
 
 module.exports = router;
