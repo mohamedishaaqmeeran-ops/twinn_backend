b5efa1646cc88e6806c3da6d1e958f9e23fd8233
unknown <mohamedishaaqmeeran@gmail.com>
2026-07-08 12:23:00 +0530

    updates

--- src/modules/payment/payment.routes.js
+++ src/modules/payment/payment.routes.js
@@ -1,18 +1,20 @@
+// src/modules/payment/payment.routes.js
+
 const express = require("express");
 const router = express.Router();
 
 const paymentController = require("./payment.controller");
-const authMiddleware = require("../../middleware/auth.middleware");
+const { protect } = require("../../middleware/auth.middleware");
 
 router.post(
   "/create-checkout",
-  authMiddleware.protect,
+  protect,
   paymentController.createCheckout
 );
 
 router.post(
   "/razorpay/verify",
-  authMiddleware.protect,
+  protect,
   paymentController.verifyRazorpay
 );
 
