7bc1c76aebb60cf16ae04dab8b93727f01b78f46
unknown <mohamedishaaqmeeran@gmail.com>
2026-07-19 18:42:36 +0530

    update

--- src/modules/twins/twin.routes.js
+++ src/modules/twins/twin.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("./twin.controller");
-const upload = require("../../middleware/upload.middleware");
+const upload = require("../../config/twinUpload");
 const { protect } = require("../../middleware/auth.middleware");
 
 router.use(protect);
